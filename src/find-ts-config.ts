d467842c7c5d812d6993d94c458d253160774059
Zachary Snow <zachary.snow@gmail.com>
2025-01-04 16:31:09 +0100

    Fix type check errors.

--- src/find-ts-config.ts
+++ src/find-ts-config.ts
@@ -4,19 +4,20 @@ import { exit } from "./lib/os.ts";
 import { resolve } from "./lib/path.ts";
 import { findTSConfig } from "./lib/utils.ts";
 
-const args = parseArgs(process.argv.slice(2), {});
+const args = parseArgs(process.argv.slice(2), {
+  positional: {
+    key: "directory",
+    single: true,
+  },
+  options: {},
+});
 
-if (args._[0] === undefined) {
+if (args.directory === "") {
   logError("Please provide a directory from which to start the search.");
   exit(1);
 }
 
-if (args._.length > 1) {
-  logError("Only one directory should be provided.");
-  exit(1);
-}
-
-const result = await findTSConfig(resolve(args._[0]));
+const result = await findTSConfig(resolve(args.directory));
 
 if (result !== null) {
   console.info(result);
--- src/install.ts
+++ src/install.ts
@@ -4,11 +4,11 @@ import { join } from "./lib/path.ts";
 
 export interface InstallArgs {}
 
-export function parseInstallArgs(args: string[]): InstallArgs {
+export function parseInstallArgs(_args: string[]): InstallArgs {
   return {};
 }
 
-export async function install(args: InstallArgs): Promise<void> {
+export async function install(_args: InstallArgs): Promise<void> {
   const pnpmExePath = join([getNodeModulesPath(), ".bin", "pnpm"]);
 
   await exec([pnpmExePath, "install"], {
