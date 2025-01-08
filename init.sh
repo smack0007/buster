f737ca3093da9ba1d67ca273a3192b7643a14860
smack0007 <zachary.snow@gmail.com>
2025-01-08 08:30:23 +0100

    init.sh now does install on testdata projects.

--- init.sh
+++ init.sh
@@ -2,3 +2,11 @@
 set -eu
 BUSTER_PATH="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 BUSTER_VERBOSE=1 ${BUSTER_PATH}/bin/buster
+
+echo "=== clicker ==="
+cd "${BUSTER_PATH}/testdata/clicker"
+BUSTER_VERBOSE=1 ${BUSTER_PATH}/bin/buster install
+
+echo "=== hello-world ==="
+cd "${BUSTER_PATH}/testdata/hello-world"
+BUSTER_VERBOSE=1 ${BUSTER_PATH}/bin/buster install
