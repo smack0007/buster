619a32cd7f20f906e85f0a4b16bb7e67884e5fa1
Zachary Snow <zachary.snow@gmail.com>
2025-07-04 22:48:59 +0200

    Map node_modules/.bin

--- docker-run.sh
+++ docker-run.sh
@@ -15,4 +15,4 @@ fi
 DOCKER_CMD=${1:-/bin/bash}
 
 cd ${BUSTER_REPO_PATH}
-docker run ${DOCKER_FLAGS} ${BUSTER_CONTAINER_NAME}:${BUSTER_CONTAINER_TAG} /bin/bash -c "ln -s /usr/local/buster/ext/* /app/ext/ && ln -s /usr/local/buster/node_modules/* /app/node_modules/ && ${DOCKER_CMD}"
+docker run ${DOCKER_FLAGS} ${BUSTER_CONTAINER_NAME}:${BUSTER_CONTAINER_TAG} /bin/bash -c "ln -s /usr/local/buster/ext/* /app/ext/ && ln -s /usr/local/buster/node_modules/* /app/node_modules/ && ln -s /usr/local/buster/node_modules/.bin /app/node_modules/.bin && ${DOCKER_CMD}"
